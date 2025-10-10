a02e203cfe038716f65292b249ba1fefa7a076a6
AngelTorrezzz <aata030115@gs.utm.mx>
2025-10-10 11:53:38 -0600

    Subiendo 9

--- Practicas/Parcial II/convolucion_correlacion.m
+++ Practicas/Parcial II/convolucion_correlacion.m
@@ -6,30 +6,25 @@ J = double(ImgOrg);
 
 %Filtro de caja de 3x3
 w1 = [1 1 1; 1 1 1; 1 1 1]; %Suaviza
-w2 = [-1 -1 -1; -1 -1 -1; -1 -1 -1]; %
+w2 = [-1 -1 -1; -1 -1 -1; -1 -1 -1]; %Realza
 
 %Convolución
-Convolucion1 = imfilter(J,w1,0,'conv')*1/9; 
-%imtool(Convolucion1,[]);
-Convolucion2 = imfilter(J,w2,0,'conv')*1/9; 
-%imtool(Convolucion2,[]);
+ConvolucionSuavizante = imfilter(J,w1,0,'conv')*1/9; 
+ConvolucionRealzante = imfilter(J,w2,0,'conv')*1/9; 
 
 %Correlación
-Correlacion1 = imfilter(J,w1)*1/9;
-%imtool(Correlacion1,[]);
-Correlacion2 = imfilter(J,w2)*1/9;
-%imtool(Correlacion2,[]);
+CorrelacionSuviazante = imfilter(J,w1)*1/9;
+CorrelacionRealzante = imfilter(J,w2)*1/9;
 
 %Convolucion, copia borde de la imagen
-ConvolucionR = imfilter(J,w,'replicate', 'conv')*1/9;
-%imtool(ConvolucionR,[]);
+ConvolucionBorde = imfilter(J,w1,'replicate', 'conv')*1/9;
 
 % Desplegar varias imagenes
 figure;
 subplot(3,2,1); imshow(ImgOrg,[]); title('Orginal');
-subplot(3,2,2); imshow(Convolucion1,[]); title('Convolucion suavizante');
-subplot(3,2,3); imshow(Convolucion2,[]); title('Convolucion no suavizante');
-subplot(3,2,4); imshow(Correlacion1,[]); title('Correlacion suavizante');
-subplot(3,2,5); imshow(Correlacion2,[]); title('Correlacion no suavizante');
-subplot(3,2,6); imshow(ConvolucionR,[]); title('Convolucion Borde de Imagen');
+subplot(3,2,2); imshow(ConvolucionSuavizante,[]); title('Convolucion Suavizante');
+subplot(3,2,3); imshow(ConvolucionRealzante,[]); title('Convolucion Realzante');
+subplot(3,2,4); imshow(CorrelacionSuviazante,[]); title('Correlacion Suavizante');
+subplot(3,2,5); imshow(CorrelacionRealzante,[]); title('Correlacion Realzante');
+subplot(3,2,6); imshow(ConvolucionBorde,[]); title('Convolucion Borde de Imagen');
 
