9eead7da2f069db124c0cf660afd08b7a18fc0d2
Angel Alvarado <aata030115@gs.utm.mx>
2025-10-28 11:53:04 -0600

    subiendo 24

--- Practicas/Parcial II/Parcial2Practica4.m
+++ Practicas/Parcial II/Parcial2Practica4.m
@@ -14,17 +14,35 @@ ConvolucionSuavizante = imfilter(J,w1,0,'conv')*1/9;
 MascaraDetalles = J - ConvolucionSuavizante;
 
 %Aplicar filtrado High-Boost
-k = 100; % Factor de refuerzo
-HighBoost = J + (k) * MascaraDetalles;
+k1 = 1; % Factor de refuerzo
+HighBoost1 = J + (k1) * MascaraDetalles;
+
+k10 = 10; % Factor de refuerzo
+HighBoost10 = J + (k10) * MascaraDetalles;
+
+k100 = 100; % Factor de refuerzo
+HighBoost100 = J + (k100) * MascaraDetalles;
 
 %Normalizar para visualización
 ImgSuavizada = uint8(ConvolucionSuavizante);
 ImgMascara = uint8(MascaraDetalles);
-ImgHighBoost = uint8(HighBoost);
+ImgHighBoost1 = uint8(HighBoost1);
+ImgHighBoost10 = uint8(HighBoost10);
+ImgHighBoost100 = uint8(HighBoost100);
 
 %Resultados
 figure;
 subplot(2,2,1), imshow(uint8(J)), title('Imagen Original');
-subplot(2,2,2), imshow(ImgSuavizada), title('Imagen Suavizada');
-subplot(2,2,3), imshow(ImgMascara), title('Máscara de Detalles');
-subplot(2,2,4), imshow(ImgHighBoost), title(['Imagen High-Boost (k = ', num2str(k), ')']);
+subplot(2,2,2), imshow(ImgHighBoost1), title(['Imagen High-Boost (k = ', num2str(k1), ')']);
+subplot(2,2,3), imshow(ImgHighBoost10), title(['Imagen High-Boost (k = ', num2str(k10), ')']);
+subplot(2,2,4), imshow(ImgHighBoost100), title(['Imagen High-Boost (k = ', num2str(k100), ')']);
+
+
+%Resultados
+%figure;
+%ubplot(3,2,1), imshow(uint8(J)), title('Imagen Original');
+%subplot(3,2,2), imshow(ImgSuavizada), title('Imagen Suavizada');
+%subplot(3,2,3), imshow(ImgMascara), title('Máscara de Detalles');
+%subplot(3,2,4), imshow(ImgHighBoost1), title(['Imagen High-Boost (k = ', num2str(k1), ')']);
+%subplot(3,2,5), imshow(ImgHighBoost10), title(['Imagen High-Boost (k = ', num2str(k10), ')']);
+%subplot(3,2,6), imshow(ImgHighBoost100), title(['Imagen High-Boost (k = ', num2str(k100), ')']);
